f740c0b2de5b0ae410b2f45b68057444d0441f4d
gerald schenk <gschenk@yorku.ca>
2016-08-05 18:19:46 -0400

    adds permutation function used for multinomial sums
    original commit message:
    commit 06b3f8a57675ec061764c1a6b6bffc64cbd7e384
    Author: gerald schenk <gschenk@yorku.ca>
    Date:   Wed May 20 02:02:46 2015 -0400
    
        [Auger] all required functions added, incorrect though

--- amult.hs
+++ amult.hs
@@ -38,7 +38,7 @@ orgData ds = ((e,b),os)
 
 
 -------- ======== Auger probabilities ======== --------
--- mathcal P (k,m,a)
+-- mathcal P (k,a,m)
 pA :: (Integral a, Fractional b) => a -> a -> b -> b
 pA 0 0 m =1
 pA 0 1 m =0
@@ -90,9 +90,10 @@ facPi k= ((*) b . (^^k) . po )
 --expression bA q n ms a p
 multProd :: (Eq a, Fractional a) => Bool -> Int -> [Int] -> [a] -> a
 multProd bA q is ps
-    | q == (sum is) && isA /= 0  = ((*isA) . prod) ps
-    | length is /= length ps = error "Unfortunately, array lenghts of indices and probs in function 'sumTerm' do not match."
-    | otherwise                  =0
+    | q == (sum is) && isA /= 0 = ((*isA) . prod) ps
+    | not bA && a /= 0          =0
+    | length is /= 1+length ps    = error "Unfortunately, array lenghts of indices and probs in function 'sumTerm' do not match."
+    | otherwise                 =0
     where   a = last is
             n = head is
             m = (sum . tail) nms
@@ -104,31 +105,29 @@ multProd bA q is ps
 
 -- permutate indices
 prmSeeds :: (Eq a, Num t, Num a) => a -> [[t]]
-prmSeeds q   | q==0  = [zs]
+prmSeeds q  | q==0  = [zs]
             | q==1  = map cstr [[1,1,0]]
             | q==2  = map cstr [[2,2,0],[1,0,1]]
             | q==3  = map cstr [[3,3,0],[2,1,1]]
             | q==4  = map cstr [[4,4,0],[3,2,1],[2,0,2]]
-            | q==5  = map cstr [[4,3,1],[3,1,2]]
-            | q==6  = map cstr [[4,2,2],[3,0,3]]
-            | q==7  = map cstr [[4,1,3]]
-            | q==8  = map cstr [[4,0,4]]
-            where zs = [0,0,0,0] -- empty list
-                  os = [1,1,1,1]
-                  ts = [2,2,2,2]
+            | q==5  = map cstr [[5,5,0],[4,3,1],[3,1,2]]
+            | q==6  = map cstr [        [5,4,1],[4,2,2],[3,0,3]]
+            | q==7  = map cstr [                [5,5,1],[4,1,3]]
+            | q==8  = map cstr [                        [5,2,3],[4,0,4]]
+            | otherwise = error "q must not exceed 8"
+            where zs = [0,0,0,0,0] -- empty list
+                  os = [1,1,1,1,1]
+                  ts = [2,2,2,2,2]
                   cstr [k,l,m] = (drop k) zs ++ (take l) os ++ (take m) ts
 
 -- permutate indices step two
 prmInds :: (Integral a) => a -> [[Int]]
-prmInds  = concat . map (nub . permutations) . prmSeeds
+prmInds  = concat . map (nub . filter f . permutations) . prmSeeds
+    where f xs = head xs >= last xs  -- filter out permutations where a exceeds n
 
--- add auger probs
-indsA q a 
-    | q >= a    =(map (++[a]) . prmInds) (q-a)
-    eeds
-
-    -- add auger probs
-
+multSum q ps = (sum . map (`f` ps) . prmInds) q
+    where   f  = multProd bA q
+            bA = True
 
 
 -- example input line
